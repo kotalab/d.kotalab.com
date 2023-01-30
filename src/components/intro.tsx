08fc79e6b68ca1f43dd219fd5ce37d8d164c8d9a
kotalab <info@kotalab.com>
2023-01-30 22:08:41 +0900

    use Constant

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -1,8 +1,10 @@
+import { BLOG_TITLE } from "../lib/constants"
+
 const Intro = () => {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        こたらの日記
+        {BLOG_TITLE}
       </h1>
     </section>
   )
