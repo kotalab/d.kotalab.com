461131a31ea0f96191f324be9d1fe66d2745caec
kotalab <info@kotalab.com>
2023-02-13 06:50:07 +0900

    move MoreStories

--- src/components/more-stories/index.tsx
+++ src/components/more-stories/index.tsx
@@ -1,5 +1,6 @@
-import PostPreview from './post-preview'
-import type Post from '../interfaces/post'
+import styles from './more-stories.module.css'
+import PostPreview from '../post-preview'
+import type Post from '../../interfaces/post'
 
 type Props = {
   posts: Post[]
@@ -8,10 +9,10 @@ type Props = {
 const MoreStories = ({ posts }: Props) => {
   return (
     <section>
-      <h2 className="mb-8 text-5xl md:text-7xl font-bold tracking-tighter leading-tight">
+      <h2 className={styles.heading}>
         More Stories
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+      <div className={styles.posts}>
         {posts.map((post) => (
           <PostPreview
             key={post.slug}
