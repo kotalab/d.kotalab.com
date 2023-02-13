44ac417ab163f491ae283e12db7540cec7634dce
kotalab <info@kotalab.com>
2023-02-13 09:52:58 +0900

    move PostTitle

--- src/components/post-title/index.tsx
+++ src/components/post-title/index.tsx
@@ -1,3 +1,4 @@
+import styles from './post-title.module.css'
 import { ReactNode } from 'react'
 
 type Props = {
@@ -6,7 +7,7 @@ type Props = {
 
 const PostTitle = ({ children }: Props) => {
   return (
-    <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left">
+    <h1 className={styles.heading}>
       {children}
     </h1>
   )
