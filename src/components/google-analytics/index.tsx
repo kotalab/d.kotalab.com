400fefa0a0ab007ca65fd07e8828da8f64e0b7ab
kotalab <info@kotalab.com>
2023-02-28 06:36:53 +0900

    fix tracking

--- src/components/google-analytics/index.tsx
+++ src/components/google-analytics/index.tsx
@@ -1,40 +1,53 @@
-import { usePathname, useSearchParams } from 'next/navigation'
-import Script from 'next/script'
-import { useEffect } from 'react'
+import { useRouter } from "next/router";
+import Script from "next/script";
+import { useEffect } from "react";
 
-const GA_TRACKING_ID = process.env.NODE_ENV === "production" ? 'UA-32395633-1' : ""
+const GA_TRACKING_ID =
+  process.env.NODE_ENV === "production" ? "UA-32395633-1" : "";
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 const pageview = (url: string) => {
-  window.gtag('config', GA_TRACKING_ID, {
+  window.gtag("config", GA_TRACKING_ID, {
     page_path: url,
-  })
-}
+  });
+};
 
 const GoogleAnalytics = () => {
-  if (!GA_TRACKING_ID) { return }
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
+  const router = useRouter();
+
   useEffect(() => {
-    const url = pathname + searchParams.toString()
-    pageview(url)
-  }, [pathname, searchParams])
+    const handleRouteChange = (url: string) => {
+      pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <>
-      <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
-      <Script strategy="afterInteractive" id="google-analytics">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${GA_TRACKING_ID}', {
-            page_path: window.location.pathname,
-          });
-        `}
-      </Script>
+      {GA_TRACKING_ID && (
+        <>
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <Script strategy="afterInteractive" id="google-analytics">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+            `}
+          </Script>
+        </>
+      )}
+      ()
     </>
-  )
-}
+  );
+};
 
-export default GoogleAnalytics
+export default GoogleAnalytics;
